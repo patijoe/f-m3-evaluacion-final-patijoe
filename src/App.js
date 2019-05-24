7f6ea4a2d5d117ac3981056394f61a98800c5c9e
Patricia Garcia <patijoe33@gmail.com>
2019-05-24 09:03:11 +0200

    add fetch component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import './App.css';
+import {petition} from './services/Petition';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      characters:[]
+    }
+  }
+
+  componentDidMount() {
+    this.fetchPetition();
+  }
+
+  fetchPetition() {
+    petition()
+    .then(data => {
+      const newCharacters = data.map((item, index) => {
+        return {...item, id: index}
+      });
+
+        this.setState({
+          characters: newCharacters
+        })
+      
+      console.log('**', newCharacters)
+    })
+  }
+
   render() {
     return (
       <div className="App">
