b4695ab6a55d1a1fb93a4130f297a44a69ede5a7
Patricia Garcia <patijoe33@gmail.com>
2019-05-24 09:28:38 +0200

    add filter name

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state={
-      characters:[]
+      characters:[],
+      filterName: ''
     }
+
+    this.handleFilterName = this.handleFilterName.bind(this);
   }
 
   componentDidMount() {
@@ -30,14 +34,28 @@ class App extends React.Component {
     })
   }
 
+  handleFilterName(event){
+    const valueName = event.currentTarget.value;
+
+    this.setState ({
+      filterName: valueName
+    })
+  }
+
   render() {
 
-    const {characters} = this.state;
+    const {characters, filterName} = this.state;
 
     return (
       <div className="App">
+
+        <label htmlFor="filter_name">Busca tu personaje preferido por nombre</label>
+        <input id="filter_name" type="text" className="filter__name" onChange={this.handleFilterName} />
+
         <ul className="characters__list">
-          {characters.map(item => {
+          {characters
+          .filter(item => item.name.includes(filterName))
+          .map(item => {
             return(
               <li className="user__item" key={item.id}>
                 <img src={item.image} alt={`imagen de ${item.name}`} className="item__img"/>
