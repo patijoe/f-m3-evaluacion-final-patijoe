2e46ec105a00e55e156891e216c40cac8d7fe955
Patricia Garcia <patijoe33@gmail.com>
2019-05-26 13:38:32 +0200

    Add docs file

---
+++ docs/precache-manifest.bc04aaddde538838934e9efdd817090d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4910b1250cdb1309b9936c595bf5e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49062e9d9ff2cf828151",
+    "url": "./static/css/main.11ffb39f.chunk.css"
+  },
+  {
+    "revision": "61d1e1698a9496e79da8",
+    "url": "./static/js/2.1334433a.chunk.js"
+  },
+  {
+    "revision": "49062e9d9ff2cf828151",
+    "url": "./static/js/main.56713106.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
